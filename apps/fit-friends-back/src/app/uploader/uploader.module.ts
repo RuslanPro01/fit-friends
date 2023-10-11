5e991bfa151f64adb2b66aff19e9aa79f7931dc5
Ruslan Shevchenko <ruslan91999@gmail.com>
2023-10-11 23:44:58 +0300

    Очистит код

--- apps/fit-friends-back/src/app/uploader/uploader.module.ts
+++ apps/fit-friends-back/src/app/uploader/uploader.module.ts
@@ -1,11 +1,10 @@
-import { Logger, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { UploaderController } from './src/uploader.controller';
 import { UploaderService } from './src/uploader.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { ConfigService } from '@nestjs/config';
 import { ConfigUploaderModule } from '@fit-friends/configs/config-uploader';
 import { UploaderRepository } from './src/uploader.repository';
-import { resolve } from 'node:path';
 
 @Module({
   imports: [
